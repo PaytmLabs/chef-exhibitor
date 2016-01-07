8f53539706861a8595d0446dc2a3746a29fd4a37
Joe Hohertz <jhohertz@gmail.com>
2016-01-07 16:29:09 -0500

    change runit pin to known stable version

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ version          '0.7.1'
 
 depends          'build-essential', '~> 2.2'
 depends          'java', '~> 1.35'
-depends          'runit', '~> 1.7'
+depends          'runit', '~> 1.5.18'
 depends          'zookeeper', '~> 3.0'
